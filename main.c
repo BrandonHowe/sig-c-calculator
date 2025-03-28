fbb1bd2c46bcb1172accc46844115c23525707b8
Brandon Howe <brandon.howe006@gmail.com>
2025-03-28 14:32:48 -0400

    update for week 3

--- main.c
+++ main.c
@@ -1,57 +1,47 @@
-#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 typedef struct StringView
 {
-    char* buffer;
-    int len;
+    int length;
+    const char* buffer;
 } StringView;
 
-StringView read_file(const char* file_name)
+StringView read_file(const char* file_path)
 {
-    FILE* file = fopen(file_name, "r"); // Open up our file for reading
-    if (!file) // If the file isn't opened properly (file is nullptr), return
+    FILE* file = fopen(file_path, "r"); // Open the file path in read mode
+    if (file == NULL)
     {
-        printf("Error opening file\n");
+        printf("File could not be opened!\n");
         return (StringView){ 0 };
     }
 
-    fseek(file, 0, SEEK_END); // Move the "cursor" to the end of the file
-    int size = ftell(file); // Ask for the current position of the "cursor"
-    rewind(file); // Move the "cursor" back to the beginning
+    fseek(file, 0, SEEK_END); // Navigate to the end of the file
+    int len = ftell(file); // Ask windows where we are
+    rewind(file);
 
-    // Allocate enough memory to store the file data, plus 1 byte for the null terminator
-    // Remember to free the memory when we're done using it! It won't be in this function
-    char* buffer = malloc(size + 1);
-    if (!buffer) // Make sure memory allocation succeeds
-    {
-        printf("Malloc call failed");
-        fclose(file);
-        return (StringView){ 0 };
-    }
+    char* buffer = malloc(len + 1);
 
-    fread(buffer, 1, size, file); // From file, read in size characters, each character taking 1 byte, outputting it into buffer
-    buffer[size] = '\0'; // Make sure the last character is a null terminator
+    fread(buffer, 1, len, file);
 
-    fclose(file); // Close our file handle when we're done with it
-    return (StringView) {
-        .buffer = buffer,
-        .len = size
+    StringView view = (StringView){
+        .length = len,
+        .buffer = buffer
     };
+
+    fclose(file);
+    buffer[len] = 0;
+
+    return view;
 }
 
 typedef enum TokenType
 {
-    TokenTypeNull,
     TokenTypeInteger,
     TokenTypePlus,
     TokenTypeMinus,
     TokenTypeTimes,
-    TokenTypeDivide,
-    TokenTypeParenOpen,
-    TokenTypeParenClose,
-    TokenTypeIdentifier
+    TokenTypeDivide
 } TokenType;
 
 typedef struct Token
@@ -62,72 +52,108 @@ typedef struct Token
 
 typedef struct TokenizerOutput
 {
-    Token* tokens;
     int token_count;
-
-    int chars_consumed;
-
-    StringView error;
+    Token* tokens;
 } TokenizerOutput;
 
-void add_token(StringView* code, TokenizerOutput* output, TokenType type, int token_length)
-{
-    // Generate the string for the token
-    StringView token_str = (StringView){ .buffer = code->buffer, .len = token_length };
-    // Generate the token itself
-    Token token = (Token){ .type = type, .text = token_str };
-    // Add the token to the output
-    output->tokens[output->token_count] = token;
-    output->token_count += 1;
-    // Remove the token from our code
-    code->buffer += token_length;
-    code->len -= token_length;
-}
-
-// Converts code into a sequence of tokens
 TokenizerOutput tokenize(StringView code)
 {
-    Token* tokens = malloc(1000 * sizeof(Token));
     TokenizerOutput output = (TokenizerOutput) {
-        .tokens = tokens,
         .token_count = 0,
-        .chars_consumed = 0,
-        .error = (StringView){ 0 }
+        .tokens = malloc(sizeof(Token) * 1000)
     };
 
-    // Keep going until we hit the end of the file
-    while (code.len > 0)
+    while (code.length > 0) // We have characters left to handle
     {
         char c = code.buffer[0];
-        switch (c)
+        if (c == '+')
         {
-        case '+': add_token(&code, &output, TokenTypePlus, 1); break;
-        case '-': add_token(&code, &output, TokenTypeMinus, 1); break;
-        case '*': add_token(&code, &output, TokenTypeTimes, 1); break;
-        case '/': add_token(&code, &output, TokenTypeDivide, 1); break;
-        case '(': add_token(&code, &output, TokenTypeParenOpen, 1); break;
-        case ')': add_token(&code, &output, TokenTypeParenClose, 1); break;
-        default:
-            {
-                if (c >= '0' && c <= '9')
-                {
-                    // We want to find the length of the number
-                    int number_len = 0;
-                    // As long as the character is 0-9, add to the number length
-                    while (number_len < code.len && code.buffer[number_len] >= '0' && code.buffer[number_len] <= '9')
-                    {
-                        number_len += 1;
-                    }
-
-                    // Now add the number token based on how long it is
-                    add_token(&code, &output, TokenTypeInteger, number_len);
-                    break;
+            // Add the token to our list
+            output.tokens[output.token_count] = (Token){
+                .type = TokenTypePlus,
+                .text = (StringView){
+                    .length = 1,
+                    .buffer = code.buffer
                 }
-                // If all else fails, we advance forwards by 1 character
-                code.buffer += 1;
-                code.len -= 1;
-                break;
+            };
+            output.token_count += 1;
+
+            // Increment the buffer by 1
+            code.buffer += 1;
+            code.length -= 1;
+        }
+        else if (c == '-')
+        {
+            // Add the token to our list
+            output.tokens[output.token_count] = (Token){
+                .type = TokenTypeMinus,
+                .text = (StringView){
+                    .length = 1,
+                    .buffer = code.buffer
+                }
+            };
+            output.token_count += 1;
+
+            // Increment the buffer by 1
+            code.buffer += 1;
+            code.length -= 1;
+        }
+        else if (c == '*')
+        {
+            // Add the token to our list
+            output.tokens[output.token_count] = (Token){
+                .type = TokenTypeTimes,
+                .text = (StringView){
+                    .length = 1,
+                    .buffer = code.buffer
+                }
+            };
+            output.token_count += 1;
+
+            // Increment the buffer by 1
+            code.buffer += 1;
+            code.length -= 1;
+        }
+        else if (c == '/')
+        {
+            // Add the token to our list
+            output.tokens[output.token_count] = (Token){
+                .type = TokenTypeDivide,
+                .text = (StringView){
+                    .length = 1,
+                    .buffer = code.buffer
+                }
+            };
+            output.token_count += 1;
+
+            // Increment the buffer by 1
+            code.buffer += 1;
+            code.length -= 1;
+        }
+        else if (c >= '0' && c <= '9') // If it's a digit
+        {
+            int number_length = 0;
+            while (number_length < code.length && code.buffer[number_length] >= '0' && code.buffer[number_length] <= '9')
+            {
+                number_length += 1;
             }
+
+            output.tokens[output.token_count] = (Token){
+                .type = TokenTypeInteger,
+                .text = (StringView){
+                    .length = number_length,
+                    .buffer = code.buffer
+                }
+            };
+            output.token_count += 1;
+
+            code.buffer += number_length;
+            code.length -= number_length;
+        }
+        else // Anything else
+        {
+            code.buffer += 1;
+            code.length -= 1;
         }
     }
 
@@ -137,47 +163,45 @@ TokenizerOutput tokenize(StringView code)
 typedef struct ASTNode
 {
     TokenType type;
-    int integer_value; // Only used when type is an integer
-    struct ASTNode* left; // If type is an arithmetic operation, left addend/factor
-    struct ASTNode* right; // If type is an arithmetic operation, right addend/factor
+    int integer_value;
+    struct ASTNode* left;
+    struct ASTNode* right;
 } ASTNode;
 
-typedef struct TokenParser
+typedef struct Parser
 {
-    TokenizerOutput* tokenizer; // List of tokens that we got from lexing
-    int current_token; // Which token we're currently looking at
-} TokenParser;
+    TokenizerOutput* tokenizer_output; // Output generated by the lexer
+    int current_token_index; // What token we're currently looking at
+} Parser;
 
-// Looks at the next token, but doesn't get rid of it yet
-Token* peek(TokenParser* parser) {
-    // If we've reached the end, return NULL (a pointer that points to nothing)
-    if (parser->current_token >= parser->tokenizer->token_count)
-    {
-         return NULL;
-    }
-    // Otherwise, we return the address of the next token
-    return &parser->tokenizer->tokens[parser->current_token];
-}
-
-// Looks at the next token and gets rid of it
-Token* consume(TokenParser* parser) {
-    // If we've reached the end, return NULL (a pointer that points to nothing)
-    if (parser->current_token >= parser->tokenizer->token_count)
+// Looks at the current token and returns a pointer to it
+Token* peek(Parser* parser)
+{
+    if (parser->current_token_index >= parser->tokenizer_output->token_count)
     {
         return NULL;
     }
-    Token* address = &parser->tokenizer->tokens[parser->current_token]; // Get the address of the next token
-    parser->current_token += 1; // Move to the next token
-    return address; // Return the address
+
+    return &parser->tokenizer_output->tokens[parser->current_token_index];
 }
 
-ASTNode* parse_integer(TokenParser* parser);
-ASTNode* parse_mul_div(TokenParser* parser);
-ASTNode* parse_add_sub(TokenParser* parser);
+// Look at the current token, and then move to the next one
+Token* consume(Parser* parser)
+{
+    if (parser->current_token_index >= parser->tokenizer_output->token_count)
+    {
+        return NULL;
+    }
 
-// Requests memory for a new node, then stores the node data
-ASTNode* create_node(TokenType type, int value, ASTNode* left, ASTNode* right) {
-    ASTNode* node = (ASTNode*)malloc(sizeof(ASTNode));
+    Token* token_address = &parser->tokenizer_output->tokens[parser->current_token_index]; // Look at the current token
+    parser->current_token_index += 1; // Move to the next token
+    return token_address;
+}
+
+ASTNode* create_node(TokenType type, int value, ASTNode* left, ASTNode* right)
+{
+    ASTNode* node = malloc(sizeof(ASTNode)); // Ask the computer for somewhere to store the node
+    if (node == NULL) return NULL; // Not really necessary but sometimes nice to have
     node->type = type;
     node->integer_value = value;
     node->left = left;
@@ -185,97 +209,47 @@ ASTNode* create_node(TokenType type, int value, ASTNode* left, ASTNode* right) {
     return node;
 }
 
-ASTNode* parse_integer(TokenParser* parser) {
+ASTNode* parse_integer(Parser* parser)
+{
     Token* token = consume(parser);
-    if (token->type == TokenTypeInteger) {
-        // We need to convert the token from a string of characters ("123") to an integer (123)
+    if (token->type == TokenTypeInteger)
+    {
+        // We need to turn a string into an integer value!
         int total = 0;
-        for (int i = 0; i < token->text.len; i++)
+        for (int i = 0; i < token->text.length; i++) // Loop over our string
         {
-            total *= 10; // Shifts the total left by 1 digit: 12 -> 120
-            total += token->text.buffer[i] - '0'; // This converts a character into a digit ('3' -> 3)
-            // In total this loop appends the next digit to the end: 12 -> 123
+            total *= 10; // Shift the total to the left by 1
+            char current_digit_char = token->text.buffer[i];
+            total += current_digit_char - '0';
         }
-        return create_node(TokenTypeInteger, total, NULL, NULL);
-    } else if (token->type == TokenTypeParenOpen) {
-        ASTNode* node = parse_add_sub(parser);
-        consume(parser); // Expect closing parenthesis
-        return node;
+        return create_node(token->type, total, NULL, NULL);
     }
     return NULL;
 }
 
-ASTNode* parse_mul_div(TokenParser* parser) {
-    // Node starts off as an integer (the first factor in our multiplication)
-    ASTNode* node = parse_integer(parser);
-    Token* next_token = peek(parser); // Look at the next token, but don't consume it yet
-    // If the next token is a times or a divide, we convert node into a mul/div operation.
-    // Otherwise, we don't do anything and it stays an integer
-    while (next_token && (next_token->type == TokenTypeTimes || next_token->type == TokenTypeDivide)) {
-        next_token = consume(parser); // Now that we know the next token is a mul/div, we can consume it
-        // Create a multiplication/division where the left is the first factor,
-        // and the right is the next integer in the token list
-        node = create_node(next_token->type, 0, node, parse_integer(parser));
-        next_token = peek(parser); // Check for more * or / (e.g. 1 * 2 * 3)
-    }
-    return node;
-}
-
-ASTNode* parse_add_sub(TokenParser* parser) {
-    // Multiplication comes first before addition, so if the first term is a multiplication operation we do that first
-    // e.g. 1 * 2 + 3 -> the first thing is a multiplication, so we do that first
-    ASTNode* node = parse_mul_div(parser);
-    Token* next_token = peek(parser); // Look at the next token, but don't consume it yet
-    // If the next token is an addition or subtraction, we convert node into a add/sub operation.
-    // Otherwise, we don't do anything and it stays an integer
-    while (next_token && (next_token->type == TokenTypePlus || next_token->type == TokenTypeMinus)) {
-        next_token = consume(parser); // Now that we know the next token is an add/sub, we can consume it
-        // We create an addition/subtraction where the left is the first addend, but we have to check
-        // if the right is multiplication, so we can handle 1 + 2 * 3. For that expression, node would
-        // be 1, and parse_mul_div(parser) would handle the 2 * 3
-        node = create_node(next_token->type, 0, node, parse_mul_div(parser));
-        next_token = peek(parser); // Check for more + or - (e.g. 1 + 2 + 3)
-    }
-    return node;
-}
-
-int evaluate_node(ASTNode node)
+ASTNode* parse_mul_div(Parser* parser)
 {
-    switch (node.type)
-    {
-    case TokenTypeNull:
-        assert(0 && "Null token type found");
-        return 0;
-    case TokenTypeInteger:
-        return node.integer_value;
-    case TokenTypePlus:
-       return evaluate_node(*node.left) + evaluate_node(*node.right);
-    case TokenTypeMinus:
-       return evaluate_node(*node.left) - evaluate_node(*node.right);
-    case TokenTypeTimes:
-       return evaluate_node(*node.left) * evaluate_node(*node.right);
-    case TokenTypeDivide:
-       return evaluate_node(*node.left) / evaluate_node(*node.right);
-    case TokenTypeIdentifier:
-        break;
-    default:
-        break;
-    }
-    return 0;
+
+}
+
+ASTNode* parse_add_sub(Parser* parser)
+{
+
 }
 
 int main(void)
 {
-    StringView content = read_file("program.sigc");
-    TokenizerOutput output = tokenize(content);
+    StringView file = read_file("/Users/brandonhowe/Documents/Projects/SIGC-live-calculator/program.sigc");
+    TokenizerOutput tokenizer_output = tokenize(file);
 
-    TokenParser parser = (TokenParser){ .tokenizer = &output, 0 };
-    ASTNode* node = parse_add_sub(&parser);
-    int result = evaluate_node(*node);
+    Parser parser = (Parser){
+        .tokenizer_output = &tokenizer_output,
+        .current_token_index = 0 // We start at the beginning
+    };
 
-    printf("Program result: %i\n", result);
+    ASTNode* int_node = parse_integer(&parser);
+    ASTNode* broken_node = parse_integer(&parser);
+    ASTNode* int_node_2 = parse_integer(&parser);
 
-    // Free the file buffer now that we're done using it
-    free(content.buffer);
     return 0;
 }
